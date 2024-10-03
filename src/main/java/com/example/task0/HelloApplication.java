ef784db607ffe5e601650d3fbfcdfdef514ef5a0
SirBrusEd2 <andreyvasiliev8952@mail.ru>
2024-10-03 08:55:53 +0300

    всё готово

--- src/main/java/com/example/task0/HelloApplication.java
+++ src/main/java/com/example/task0/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.task0;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,9 +11,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // Загружаем FXML файл
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 500, 600);
+
+        stage.setTitle("Чаевые");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/task0/HelloController.java
+++ src/main/java/com/example/task0/HelloController.java
@@ -2,13 +2,52 @@ package com.example.task0;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField inputField;
+    @FXML
+    private TextField inputprocentField;
+    @FXML
+    private TextField resultField;
+    @FXML
+    private TextField totalField; // Для отображения полной суммы
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void calculateBigTip() {
+        calculateTip(15);
+    }
+
+    @FXML
+    private void calculateNormalTip() {
+        try {
+            double billAmount = Double.parseDouble(inputprocentField.getText());
+            if (billAmount == 10 || billAmount == 9) {
+                calculateTip(billAmount);
+            } else {
+                resultField.setText("Введите один из предложенных вариантов");
+            }
+        } catch (NumberFormatException e) {
+            resultField.setText("Ошибка ввода");
+        }
+    }
+
+    @FXML
+    private void calculateSmallTip() {
+        calculateTip(3);
+    }
+
+    private void calculateTip(double percent) {
+        try {
+            double billAmount = Double.parseDouble(inputField.getText());
+            double tipAmount = billAmount * (percent / 100);
+            double totalAmount = billAmount + tipAmount; // Полная сумма
+            resultField.setText(String.format("%.2f", tipAmount));
+            totalField.setText(String.format("%.2f", totalAmount)); // Обновление поля с полной суммой
+        } catch (NumberFormatException e) {
+            resultField.setText("Ошибка ввода");
+        }
+
     }
 }
